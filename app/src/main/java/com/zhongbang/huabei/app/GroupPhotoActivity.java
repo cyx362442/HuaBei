36963a3491fcf32da1f03442014d96a1a0e00855
cyx362442 <chenyunyan88@aliyun.com>
2017-09-18 22:14:06 +0800

    0918

--- app/src/main/java/com/zhongbang/huabei/app/GroupPhotoActivity.java
+++ app/src/main/java/com/zhongbang/huabei/app/GroupPhotoActivity.java
@@ -1,14 +1,34 @@
 package com.zhongbang.huabei.app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.zhongbang.huabei.R;
 import com.zhongbang.huabei.fragment.dialog.GroupImageFragment;
+import com.zhongbang.huabei.http.UploadUtil;
+import com.zhongbang.huabei.utils.ShapreUtis;
+import com.zhongbang.huabei.yunmai.CameraManager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +40,11 @@ public class GroupPhotoActivity extends AppCompatActivity {
     TextView mTvTitle;
     @BindView(R.id.img_group)
     ImageView mImgGroup;
+    private String mAccount;
+
+    private final String urlCommit="http://chinaqmf.cn:8088/ihuabei/app/user/submitHandCardImg.app";
+    public static int TAKE_PHOTO_REQUEST_CODE=100;
+    public static final String strDir= Environment.getExternalStorageDirectory()+"/zhongbang/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +52,28 @@ public class GroupPhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_group_photo);
         ButterKnife.bind(this);
         mTvTitle.setText("人卡合影");
+
+        ShapreUtis shapreUtis = ShapreUtis.getInstance(this);
+        mAccount = shapreUtis.getAccount();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==TAKE_PHOTO_REQUEST_CODE){
+            final Bitmap bmp = getScaleBitmap(this, getTempImage().getPath());
+            final File file = new File(strDir, "group.jpg");
+            Glide.with(this).load(file).skipMemoryCache(true).
+                    diskCacheStrategy(DiskCacheStrategy.NONE).
+                    error(R.mipmap.id_reverse).centerCrop().into(mImgGroup);
+            //图片压缩并保有存
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    compressBmpToFile(bmp,file);
+                }
+            }).start();
+        }
     }
 
     @OnClick({R.id.img_return, R.id.rl_group, R.id.btn_commit})
@@ -40,7 +87,81 @@ public class GroupPhotoActivity extends AppCompatActivity {
                 fragment.show(getFragmentManager(),getString(R.string.groupimage));
                 break;
             case R.id.btn_commit:
+                File file = new File(strDir, "group.jpg");
+                final HashMap<String, String> map = new HashMap<>();
+                map.put("username",mAccount);
+                final Map<String, File> mapFiles = new HashMap<String, File>();
+                mapFiles.put("handCardImg",file);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String post = UploadUtil.post(urlCommit, map, mapFiles);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
                 break;
+        }
+    }
+
+    public static File getTempImage() {
+        if (android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED)) {
+            File tempFile = new File(strDir, "group.jpg");
+            try {
+                tempFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return tempFile;
+        }
+        return null;
+    }
+
+    public static Bitmap getScaleBitmap(Context ctx, String filePath) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(filePath, opt);
+
+        int bmpWidth = opt.outWidth;
+        int bmpHeght = opt.outHeight;
+
+        WindowManager windowManager = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        int screenWidth = display.getWidth();
+        int screenHeight = display.getHeight();
+
+        opt.inSampleSize = 1;
+        if (bmpWidth > bmpHeght) {
+            if (bmpWidth > screenWidth)
+                opt.inSampleSize = bmpWidth / screenWidth;
+        } else {
+            if (bmpHeght > screenHeight)
+                opt.inSampleSize = bmpHeght / screenHeight;
+        }
+        opt.inJustDecodeBounds = false;
+
+        bmp = BitmapFactory.decodeFile(filePath, opt);
+        return bmp;
+    }
+    public static void compressBmpToFile(Bitmap bmp,File file){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int options = 80;//个人喜欢从80开始,
+        bmp.compress(Bitmap.CompressFormat.JPEG, options, baos);
+        while (baos.toByteArray().length / 1024 > 100) {
+            baos.reset();
+            options -= 10;
+            bmp.compress(Bitmap.CompressFormat.JPEG, options, baos);
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/zhongbang/huabei/fragment/dialog/GroupImageFragment.java
+++ app/src/main/java/com/zhongbang/huabei/fragment/dialog/GroupImageFragment.java
@@ -2,14 +2,23 @@ package com.zhongbang.huabei.fragment.dialog;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.zhongbang.huabei.R;
+import com.zhongbang.huabei.app.GroupPhotoActivity;
 
-public class GroupImageFragment extends DialogFragment {
+import java.io.File;
+import java.io.IOException;
+
+public class GroupImageFragment extends DialogFragment implements View.OnClickListener {
+
     public static GroupImageFragment newInstance() {
         Bundle args = new Bundle();
         GroupImageFragment fragment = new GroupImageFragment();
@@ -21,8 +30,20 @@ public class GroupImageFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
         View inflate = layoutInflater.inflate(R.layout.fragment_group_image, null);
+        inflate.findViewById(R.id.btn_ok).setOnClickListener(this);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(inflate);
         return builder.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        // 指定存储照片的路径
+        Uri imageUri = Uri.fromFile(GroupPhotoActivity.getTempImage());
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        getActivity().startActivityForResult(intent, GroupPhotoActivity.TAKE_PHOTO_REQUEST_CODE);
+        dismiss();
     }
 }
