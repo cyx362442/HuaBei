a2a7fa613c18092e875ed999d6cbb059b4af5b2d
cyy <276523847@qq.com>
2017-09-28 18:23:24 +0800

    cyy09282

--- app/src/main/java/com/zhongbang/huabei/fragment/NotificationFragment.java
+++ app/src/main/java/com/zhongbang/huabei/fragment/NotificationFragment.java
@@ -1,5 +1,6 @@
 package com.zhongbang.huabei.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.VolleyError;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.zhongbang.huabei.R;
@@ -21,6 +23,7 @@ import com.zhongbang.huabei.bean.NotifMessage;
 import com.zhongbang.huabei.http.DownHTTP;
 import com.zhongbang.huabei.http.VolleyResultListener;
 import com.zhongbang.huabei.utils.ToastUtil;
+import com.zhongbang.huabei.webview.WebActivity;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -33,19 +36,19 @@ import butterknife.Unbinder;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NotificationFragment extends Fragment {
+public class NotificationFragment extends Fragment implements BaseQuickAdapter.OnRecyclerViewItemClickListener {
 
     private final String url = "http://chinaqmf.cn:8088/ihuabei/app/system/announcements.app";
+    private final String urlNotification="http://chinaqmf.cn:8088/ihuabei/attached/announcement/";
     @BindView(R.id.tv_title)
     TextView mTvTitle;
     @BindView(R.id.recycleView)
     RecyclerView mRecycleView;
     Unbinder unbinder;
-    @BindView(R.id.rl_load)
-    RelativeLayout mRlLoad;
 
     private List<NotifMessage> mList = new ArrayList<>();
     private NotifactionAdapter mAdapter;
+    private View mViewLoad;
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -58,12 +61,13 @@ public class NotificationFragment extends Fragment {
         View inflate = inflater.inflate(R.layout.fragment_notification, container, false);
         unbinder = ButterKnife.bind(this, inflate);
         mTvTitle.setText("消息中心");
+        mViewLoad = inflate.findViewById(R.id.include);
 
         mRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mAdapter = new NotifactionAdapter(mList);
         mRecycleView.setAdapter(mAdapter);
         mRecycleView.addItemDecoration(new MyDecoration(getActivity(), MyDecoration.VERTICAL_LIST));
-
+        mAdapter.setOnRecyclerViewItemClickListener(this);
         Http_Msg();
         return inflate;
     }
@@ -72,7 +76,7 @@ public class NotificationFragment extends Fragment {
         DownHTTP.getVolley(url, new VolleyResultListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mRlLoad.setVisibility(View.GONE);
+                mViewLoad.setVisibility(View.GONE);
                 ToastUtil.showShort(getActivity(),"网络异常");
             }
             @Override
@@ -80,11 +84,20 @@ public class NotificationFragment extends Fragment {
                 Gson gson = new Gson();
                 Type type = new TypeToken<List<NotifMessage>>() {
                 }.getType();
-                List<NotifMessage> list = gson.fromJson(response, type);
-                mAdapter.setNewData(list);
-                mRlLoad.setVisibility(View.GONE);
+                mList = gson.fromJson(response, type);
+                mAdapter.setNewData(mList);
+                mViewLoad.setVisibility(View.GONE);
             }
         });
+    }
+
+    @Override
+    public void onItemClick(View view, int i) {
+        NotifMessage notifMessage = mList.get(i);
+        Intent intent = new Intent(getActivity(), WebActivity.class);
+        intent.putExtra("url",urlNotification+notifMessage.getHtml());
+        intent.putExtra(getString(R.string.webtitle),notifMessage.getTitle());
+        startActivity(intent);
     }
 
     @Override
