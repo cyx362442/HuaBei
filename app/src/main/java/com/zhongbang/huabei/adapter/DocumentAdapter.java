b284338ac935c2bb713b46901442ebc1fae28ab0
cyy <276523847@qq.com>
2017-11-15 16:12:50 +0800

    cyy1103

--- app/src/main/java/com/zhongbang/huabei/adapter/DocumentAdapter.java
+++ app/src/main/java/com/zhongbang/huabei/adapter/DocumentAdapter.java
@@ -8,12 +8,14 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.zhongbang.huabei.R;
 import com.zhongbang.huabei.app.App;
 import com.zhongbang.huabei.bean.Document;
+import com.zhongbang.huabei.utils.ToastUtil;
 
 import java.util.List;
 
@@ -21,11 +23,17 @@ import java.util.List;
  * Created by Administrator on 2017-09-27.
  */
 
-public class DocumentAdapter extends BaseQuickAdapter<Document> {
+public class DocumentAdapter extends BaseQuickAdapter<Document> implements View.OnClickListener {
     private Context mContext;
+    private final PopupWindow mPopupWindow;
+
     public DocumentAdapter(List<Document> data,Context context) {
         super(R.layout.recy_document_item,data);
         mContext=context;
+        View inflate = LayoutInflater.from(mContext).inflate(R.layout.popu_item, null, false);
+        mPopupWindow = new PopupWindow(inflate,400,80,true);
+        inflate.findViewById(R.id.tv_saveImg).setOnClickListener(this);
+        inflate.findViewById(R.id.tv_copyText).setOnClickListener(this);
     }
 
     @Override
@@ -46,10 +54,17 @@ public class DocumentAdapter extends BaseQuickAdapter<Document> {
         baseViewHolder.setOnClickListener(R.id.image, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View inflate = LayoutInflater.from(mContext).inflate(R.layout.popu_item, null, false);
-                PopupWindow popupWindow = new PopupWindow(inflate,400,80,true);
-                popupWindow.showAsDropDown(view);
+                mPopupWindow.showAsDropDown(view);
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.tv_saveImg){
+            ToastUtil.showShort(mContext,"保存图片");
+        }else if(view.getId()==R.id.tv_copyText){
+            ToastUtil.showShort(mContext,"复制文案");
+        }
     }
 }
